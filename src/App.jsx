a281dd9cc61983ea918490c3402c371b835fd7ec
Largopie <106071687+Largopie@users.noreply.github.com>
2024-04-18 00:24:51 +0900

    feat: 음식점 추가 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { restaurants } from '../db.json';
 import CategoryFilter from './components/CategoryFilter/CategoryFilter';
 import Header from './components/Header/Header';
 import RestaurantInfoModal from './components/Modal/RestaurantInfoModal/RestaurantInfoModal';
-// import AddRestaurantModal from './components/Modal/AddRestaurantModal/AddRestaurantModal';
+import AddRestaurantModal from './components/Modal/AddRestaurantModal/AddRestaurantModal';
 
 import RestaurantList from './components/RestaurantList/RestaurantList';
 
 function App() {
+  const [restaurantList, setRestaurantList] = useState(restaurants);
   const [category, setCategory] = useState('전체');
-  const filteredRestaurants = category === '전체' ? restaurants : restaurants.filter((restaurant) => restaurant.category === category);
+  const filteredRestaurants = category === '전체' ? restaurantList : restaurantList.filter((restaurant) => restaurant.category === category);
 
   const [detailRestaurant, setDetailRestaurant] = useState(null);
   const [isDetailOpen, setIsDetailOpen] = useState(false);
@@ -18,16 +19,31 @@ function App() {
   const closeDetail = () => setIsDetailOpen(false);
   const openDetail = () => setIsDetailOpen(true);
 
+  const [isAddRestaurantOpen, setIsAddRestaurantOpen] = useState(false);
+
+  const closeAddRestaurant = () => setIsAddRestaurantOpen(false);
+  const openAddRestaurant = () => setIsAddRestaurantOpen(true);
+
+  const addRestaurant = (restaurant) => {
+    setRestaurantList([
+      {
+        id: Date.now(),
+        ...restaurant,
+      },
+      ...restaurantList,
+    ]);
+  };
+
   return (
     <>
-      <Header />
+      <Header openAddRestaurant={openAddRestaurant} />
       <main>
         <CategoryFilter category={category} setCategory={setCategory} />
         <RestaurantList openDetail={openDetail} setDetailRestaurant={setDetailRestaurant} restaurants={filteredRestaurants} />
       </main>
       <aside>
         {isDetailOpen && <RestaurantInfoModal restaurant={detailRestaurant} closeDetail={closeDetail} />}
-        {/* <AddRestaurantModal closeAddRestaurant={closeDetail} /> */}
+        {isAddRestaurantOpen && <AddRestaurantModal addRestaurant={addRestaurant} closeAddRestaurant={closeAddRestaurant} />}
       </aside>
     </>
   );
--- src/components/Modal/AddRestaurantModal/AddRestaurantModal.jsx
+++ src/components/Modal/AddRestaurantModal/AddRestaurantModal.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import Modal from '../Modal';
 import styles from './add-restaurant-modal.module.css';
 
-export default function AddRestaurantModal() {
+export default function AddRestaurantModal({ addRestaurant, closeAddRestaurant }) {
+  const [restaurant, setRestaurant] = useState({
+    name: '',
+    category: '',
+    description: '',
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    addRestaurant(restaurant);
+    closeAddRestaurant();
+  };
+
   return (
-    <Modal>
+    <Modal close={closeAddRestaurant}>
       <h2 className={`${styles.modalTitle} text-title`}>새로운 음식점</h2>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <div className={`${styles.formItem} ${styles.formItemRequired}`}>
           <label htmlFor='category text-caption'>카테고리</label>
-          <select name='category' id='category' required>
+          <select
+            value={restaurant.category}
+            onChange={(e) => setRestaurant({ ...restaurant, category: e.target.value })}
+            name='category'
+            id='category'
+            required
+          >
             <option value=''>선택해 주세요</option>
             <option value='한식'>한식</option>
             <option value='중식'>중식</option>
@@ -20,11 +39,18 @@ export default function AddRestaurantModal() {
         </div>
         <div className={`${styles.formItem} ${styles.formItemRequired}`}>
           <label htmlFor='name text-caption'>이름</label>
-          <input type='text' name='name' id='name' required />
+          <input type='text' name='name' id='name' value={restaurant.name} onChange={(e) => setRestaurant({ ...restaurant, name: e.target.value })} required />
         </div>
         <div className={styles.formItem}>
           <label htmlFor='description text-caption'>설명</label>
-          <textarea name='description' id='description' cols='30' rows='5'></textarea>
+          <textarea
+            name='description'
+            id='description'
+            cols='30'
+            rows='5'
+            value={restaurant.description}
+            onChange={(e) => setRestaurant({ ...restaurant, description: e.target.value })}
+          ></textarea>
           <span className={`${styles.helpText} text-caption`}>메뉴 등 추가 정보를 입력해 주세요.</span>
         </div>
         <div className={styles.buttonContainer}>
